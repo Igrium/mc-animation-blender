f0635e4f76c95a9c1652db296f8cb10cc08b4b18
Sam54123 <thesam54123@gmail.com>
2019-06-14 13:52:07 -0700

    setup workspace for blender addon

---
+++ mc_animation_blender/__init__.py
@@ -0,0 +1,34 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name" : "Minecraft Animation Tools",
+    "author" : "Igrium",
+    "description" : "Tools for animation in adventure maps",
+    "blender" : (2, 80, 0),
+    "version" : (0, 0, 1),
+    "location" : "View3D",
+    "warning" : "",
+    "category" : "Import-Export"
+}
+
+import bpy
+
+from . import operator_export_json
+
+def register():
+    print("registering mcanim")
+    operator_export_json.register()
+
+def unregister():
+    operator_export_json.unregister()
---
+++ mc_animation_blender/operator_export_json.py
@@ -0,0 +1,78 @@
+import bpy
+from . exporters import transform
+
+def write_some_data(context, filepath, use_some_setting):
+    print("running write_some_data...")
+    f = open(filepath, 'w', encoding='utf-8')
+    f.write("Hello World %s" % use_some_setting)
+    f.close()
+
+    return {'FINISHED'}
+
+
+# ExportHelper is a helper class, defines filename and
+# invoke() function which calls the file selector.
+from bpy_extras.io_utils import ExportHelper
+from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.types import Operator
+
+
+class MC_Export_Operator(Operator, ExportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "mcanim.export_anim"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Export Minecraft Animation"
+
+    # ExportHelper mixin class uses this
+    filename_ext = ".json"
+
+    filter_glob: StringProperty(
+        default="*.json",
+        options={'HIDDEN'},
+        maxlen=255,  # Max internal buffer length, longer would be clamped. 
+    )
+
+    # List of operator properties, the attributes will be assigned
+    # to the class instance from the operator settings before calling.
+    looping = BoolProperty(
+        name="Looping",
+        description="Should this animation loop?",
+        default=True,
+        )
+
+    resetWhenDone = BoolProperty(
+        name="Reset when done",
+        description="Should this reset to starting position when done?",
+        default=False,
+        )
+
+    id = StringProperty(
+        name="ID",
+        description="Unique numerical ID that Minecraft will refer to this animation by",
+        default='0',
+        )
+
+    def execute(self, context):
+        return write_some_data(context, self.filepath, self.use_setting)
+
+
+# Only needed if you want to add into a dynamic menu
+def menu_func_export(self, context):
+    self.layout.operator(MC_Export_Operator.bl_idname, text="Minecraft Animation (.json)")
+
+
+def register():
+    print("registering anim export")
+    bpy.utils.register_class(MC_Export_Operator)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
+
+
+def unregister():
+    bpy.utils.unregister_class(MC_Export_Operator)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+
+
+if __name__ == "__main__":
+    register()
+
+    # test call
+    bpy.ops.export_test.some_data('INVOKE_DEFAULT')
