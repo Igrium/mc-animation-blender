57fe78a5a9dabd90d40368d4cf6ee2af01c13087
Sam54123 <thesam54123@gmail.com>
2019-09-01 17:00:50 -0700

    incrimented version number

--- mc_animation_blender/__init__.py
+++ mc_animation_blender/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "Igrium",
     "description" : "Tools for animation in adventure maps",
     "blender" : (2, 80, 0),
-    "version" : (0, 0, 1),
+    "version" : (0, 1, 0),
     "location" : "View3D",
     "warning" : "",
     "category" : "Import-Export"
